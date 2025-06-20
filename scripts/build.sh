f699aae52c51bfb1fa141bc05cb3f64280067ea2
baneymelo <baneymelo@gmail.com>
2025-06-20 01:29:52 -0500

    project/get rid of scripts folder and ci reference modified

--- scripts/build.sh
+++
@@ -1,9 +0,0 @@
-#!bin/bash
-set -o errexit
-set -o noglob
-set -o nounset
-set -o pipefail
-
-cd"$(dirname "${0}")"
-
-../build.sh
--- scripts/test.sh
+++
@@ -1,8 +0,0 @@
-set -o errexit
-set -o noglob
-set -o nounset
-set -o pipefail
-
-cd"$(dirname "${0}")"
-
-../test.sh
