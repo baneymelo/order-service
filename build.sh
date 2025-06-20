f2dee9b63447d3ccb6e6bf5afcfdeb9c029289d1
baneymelo <baneymelo@gmail.com>
2025-06-20 01:39:11 -0500

    scripts/adding bin/bash

--- build.sh
+++ build.sh
@@ -1,1 +1,7 @@
+#!bin/bash
+set -o errexit
+set -o noglob
+set -o nounset
+set -o pipefail
+
 mvn compile package
--- test.sh
+++ test.sh
@@ -1,8 +1,7 @@
+#!bin/bash
 set -o errexit
 set -o noglob
 set -o nounset
 set -o pipefail
-
-cd"$(dirname "${0}")"
 
 mvn clean compile test
